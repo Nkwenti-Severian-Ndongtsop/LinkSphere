a615781488908de1ef0109d768774167248eccf7
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-06-03 14:00:24 +0100

    refactor(routes): update public exports in mod.rs to include specific handler functions for links and users

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -2,6 +2,6 @@ mod links;
 mod users;
 mod root;
 pub mod auth;
-pub use links::*;
-pub use users::*;
+pub use links::{get_links_handler, delete_link_handler, increment_click_count_handler, create_link_handler};
+pub use users::create_user_handler;
 pub use root::root_handler;
